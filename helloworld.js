a95817930f1d72f7b94fe7bd24e21d84194a2423
Matt Webb <matt.webb@sympoz.com>
2015-07-12 22:07:10 -0600

    initial commit

---
+++ helloworld.js
@@ -0,0 +1,32 @@
+var five = require('johnny-five');
+var temporal = require('temporal');
+var board = new five.Board();
+var events = [];
+var strobe;
+
+board.on('ready', function() {
+    strobe = new five.Pin(13);
+
+    temporal.loop(500, function(loop) {
+        strobe[loop.called % 2 === 0 ? 'high' : 'low']();
+    });
+
+
+    // Pin emits "high" and "low" events, whether it's
+    // input or output.
+    ['high', 'low'].forEach(function(state) {
+        strobe.on(state, function() {
+            if (events.indexOf(state) === -1) {
+                console.log("Event emitted for:", state, "on", this.addr);
+                events.push(state);
+            }
+        });
+    });
+
+    var analog = new five.Pin("A0");
+
+    // Query the analog pin for its current state.
+    analog.query(function(state) {
+        console.log(state);
+    });
+});
